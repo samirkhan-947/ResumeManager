9a643ba1df608c7b803bcb29f3246e9f89f87284
samirkhan-947 <samirrkhan947@gmail.com>
2023-03-07 10:39:46 +0530

    Add project files.

---
+++ ResumeManager/Controllers/ResumeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeManager.Controllers
+{
+    public class ResumeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ResumeManager/Models/Experience.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResumeManager.Models
+{
+    public class Experience
+    {
+        public Experience()
+        {
+        }
+
+        [Key]
+        public int ExperienceId { get; set; }
+
+        [ForeignKey("Applicant")]//very important
+        public int ApplicantId { get; set; }
+        public virtual Applicant Applicant { get; private set; } //very important 
+
+        public string CompanyName { get; set; }
+        public string Designation { get; set; }
+        [Required]
+        public int YearsWorked { get; set; }
+    }
+}
